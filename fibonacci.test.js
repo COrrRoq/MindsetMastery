3afc0ef374026ff78ae2534b831569057423b985
COrrRoq <catherine.orr@roq.co.uk>
2023-02-07 15:43:49 +0000

    First test

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -7,6 +7,13 @@ beforeEach(() => {
 });
 
 test('When initialized the next number can be returned', () => {
-    fibonacci.init(8);
-    expect(fibonacci.next()).toBe(13);
+   fibonacci.init(8);
+   expect(fibonacci.next()).toBe(13);
 });
+
+
+test('When "next" is used, the sequence moves to the next number', () => {
+    fibonacci.next;
+    expect(fibonacci.next());
+    toThrow();
+})
